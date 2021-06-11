e5de747cac0fda35b12c156fb65f9223b3e30293
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-11 22:26:13 +0530

    added an method to check Max number

---
+++ MaxUsingGenerics/MaxUsingGenerics/MaxNumberINT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxUsingGenerics
+{
+    class MaxNumberINT
+    {
+        public void Maximum(int firstNumber, int secondNumber,int thiredNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) == 1 & firstNumber.CompareTo(thiredNumber) == 1)
+            {
+                Console.WriteLine(firstNumber + " is greater");
+            }
+            if (secondNumber.CompareTo(firstNumber)==1 & secondNumber.CompareTo(thiredNumber) == 1)
+            {
+                Console.WriteLine(secondNumber+" is greater");
+            }
+            if (thiredNumber.CompareTo(firstNumber)==1 & thiredNumber.CompareTo(secondNumber)==1)
+            {
+                Console.WriteLine(thiredNumber + " is greater");
+            }
+        }
+    }
+}
---
+++ MaxUsingGenerics/MaxUsingGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaxUsingGenerics
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            MaxNumberINT objMax = new MaxNumberINT();
+            Console.WriteLine("Enter three numbers:");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            int thiredValue = Convert.ToInt32(Console.ReadLine());
+            objMax.Maximum(firstValue,secondValue,thiredValue);
+            Console.ReadKey();
+        }
+    }
+}
