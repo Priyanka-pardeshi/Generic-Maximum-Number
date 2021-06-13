23fdb86e9fc00650ae794f1ebb32078ed23acdb5
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-13 15:45:20 +0530

    Added an test case--3

--- MaxUsingGenerics/MaxTestCases.Test/UnitTest1.cs
+++ MaxUsingGenerics/MaxTestCases.Test/UnitTest1.cs
@@ -8,20 +8,41 @@ namespace MaxTestCases.Test
         [SetUp]
         public void Setup()
         {
-        
+
         }
         MaxNumberINT objMaxInt = new MaxNumberINT();
 
         [Test]
         public void Test1()
         {
-            int firstValue = 30, secondValue = 50, thiredValue = 100;
-            int expected = thiredValue;
+            int firstValue = 300, secondValue = 50, thiredValue = 100;
+            int expected = firstValue;
 
             int result = objMaxInt.Maximum(firstValue, secondValue, thiredValue);
 
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Test2()
+        {
+            int firstValue = 30, secondValue = 200, thiredValue = 10;
+            int expected = secondValue;
+
+            int result = objMaxInt.Maximum(firstValue, secondValue, thiredValue);
+
+            Assert.AreEqual(expected, result);
         }
 
+        [Test]
+        public void Test3()
+        {
+        int firstValue = 30;
+        int secondValue = 20;
+        int thiredValue = 100;
+        int expected = thiredValue;
+        int result = objMaxInt.Maximum(firstValue, secondValue, thiredValue);
+
+        Assert.AreEqual(expected, result);
+        }
     }
 }
