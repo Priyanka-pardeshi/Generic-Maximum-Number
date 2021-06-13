6bc5a62ef0d9cf4e1d2a0ceac9bf507de1b1ac5e
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-13 15:15:43 +0530

    Added an test case-1

--- MaxUsingGenerics/MaxUsingGenerics/Program.cs
+++ MaxUsingGenerics/MaxUsingGenerics/Program.cs
@@ -7,12 +7,12 @@ namespace MaxUsingGenerics
         public static void Main(string[] args)
         {
             MaxNumberINT objMax = new MaxNumberINT();
-            Console.WriteLine("Enter three numbers:");
-            objMax.Maximum(30,20,10);
-            //int firstValue = Convert.ToInt32(Console.ReadLine());
-            //int secondValue = Convert.ToInt32(Console.ReadLine());
-            //int thiredValue = Convert.ToInt32(Console.ReadLine());
-            //objMax.Maximum(firstValue,secondValue,thiredValue);
+            Console.WriteLine("Welcome");
+            objMax.Maximum(30, 20, 10);
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            int thiredValue = Convert.ToInt32(Console.ReadLine());
+            objMax.Maximum(firstValue, secondValue, thiredValue);
             Console.ReadKey();
         }
     }
